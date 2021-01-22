c961ca1553425274d3fabcd209e6f3dbbbd9f7ec
Philip Spelman <philip.spelman@vaporstream.com>
2021-01-22 17:16:05 -0500

    - implemented basic express server - told express how to handle static files - updated the production config to use a public path of /static/ - added a script to spin up the express server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')))
+
+app.get('/', (req, res) => {
+  const pathToIndexHtml = path.resolve(__dirname, '../dist/index.html')
+  const contentFromHtmlFile = fs.readFileSync(pathToIndexHtml, 'utf-8')
+  res.send(contentFromHtmlFile)
+  // res.send("Response to whatever") // http response object
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`listening at port ${port}`, )
+
+})
