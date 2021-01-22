2c54882df6711fda44574e71c4742b8590c8571f
Philip Spelman <philip.spelman@vaporstream.com>
2021-01-22 17:29:40 -0500

    - implemented basic server and 2 routes to serve a multi-page application

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')))
+
+app.get('/hello-world/', (req, res) => {
+  const pathToIndexHtml = path.resolve(__dirname, '../dist/hello-world.html')
+  const contentFromHtmlFile = fs.readFileSync(pathToIndexHtml, 'utf-8')
+  res.send(contentFromHtmlFile)
+  // res.send("Response to whatever") // http response object
+})
+
+app.get('/kiwi/', (req, res) => {
+  const pathToIndexHtml = path.resolve(__dirname, '../dist/kiwi.html')
+  const contentFromHtmlFile = fs.readFileSync(pathToIndexHtml, 'utf-8')
+  res.send(contentFromHtmlFile)
+  // res.send("Response to whatever") // http response object
+})
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`listening at port ${port}`, )
+
+})
